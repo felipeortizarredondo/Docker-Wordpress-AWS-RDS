3323668027fdc83a81ea0cb5c2dfb33129e090ab
Felipe Ortiz Arredondo <107318069+felipeortizarredondo@users.noreply.github.com>
2023-07-03 00:04:00 -0400

    Update config_database.sh

--- config_database.sh
+++ config_database.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Solicitar al usuario los valores de los parámetros
-read -p "Ingrese el host de la base de datos [db-wordpress.cluster-chbrcmzpmf7a.us-east-1.rds.amazonaws.com]: " db_host
-db_host=
+read -p "Ingrese el nombre del punto de enlace de la base de datos ejemplo: [db-wordpress.cluster-chbrcmzpmf7a.us-east-1.rds.amazonaws.com]" db_host
 
 # Reemplazar los valores de las variables de entorno en el Dockerfile
 sed -i "s/ENV WORDPRESS_DB_HOST=.*/ENV WORDPRESS_DB_HOST=${db_host}/" Dockerfile
