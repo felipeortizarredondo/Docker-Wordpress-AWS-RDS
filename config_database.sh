42ba50e17f8a2c3d4ea5936643e4b362e4b69b0c
Felipe Ortiz Arredondo <107318069+felipeortizarredondo@users.noreply.github.com>
2023-07-02 23:56:08 -0400

    Update config_database.sh

--- config_database.sh
+++ config_database.sh
@@ -2,21 +2,9 @@
 
 # Solicitar al usuario los valores de los parámetros
 read -p "Ingrese el host de la base de datos [db-wordpress.cluster-chbrcmzpmf7a.us-east-1.rds.amazonaws.com]: " db_host
-db_host=${db_host:-db-wordpress.cluster-chbrcmzpmf7a.us-east-1.rds.amazonaws.com}
-
-read -p "Ingrese el usuario de la base de datos [wordpressuser]: " db_user
-db_user=${db_user:-wordpressuser}
-
-read -p "Ingrese la contraseña de la base de datos [wordpresspassword]: " db_password
-db_password=${db_password:-wordpresspassword}
-
-read -p "Ingrese el nombre de la base de datos [wordpress]: " db_name
-db_name=${db_name:-wordpress}
+db_host=
 
 # Reemplazar los valores de las variables de entorno en el Dockerfile
 sed -i "s/ENV WORDPRESS_DB_HOST=.*/ENV WORDPRESS_DB_HOST=${db_host}/" Dockerfile
-sed -i "s/ENV WORDPRESS_DB_USER=.*/ENV WORDPRESS_DB_USER=${db_user}/" Dockerfile
-sed -i "s/ENV WORDPRESS_DB_PASSWORD=.*/ENV WORDPRESS_DB_PASSWORD=${db_password}/" Dockerfile
-sed -i "s/ENV WORDPRESS_DB_NAME=.*/ENV WORDPRESS_DB_NAME=${db_name}/" Dockerfile
 
 echo "¡Dockerfile actualizado exitosamente!"
